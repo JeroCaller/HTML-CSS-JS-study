c57d6cb21f7a57ab4c190dddabb53ef9eacb2777
JeroCaller <j69m17@gmail.com>
2024-02-14 13:12:22 +0900

    JS - window 객체, history 객체 활용 예제.

--- js/js-ex.js
+++
@@ -1,5 +0,0 @@
-let currentWindowWidth = window.innerWidth;
-let currentWindowHeight = window.innerHeight;
-
-document.write("현재 웹 문서 영역 너비는 " + currentWindowWidth + "px 입니다.");
-document.write("현재 웹 문서 영역 높이는 " + currentWindowHeight + "px 입니다.");
