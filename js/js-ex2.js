5821fcc11b58f98330b84798a11e2401cf9a5c98
JeroCaller <j69m17@gmail.com>
2024-02-15 22:25:40 +0900

    JS - 기존 예제 코드 수정 및 새 예제 추가.

--- js/js-ex2.js
+++ js/js-ex2.js
@@ -1,3 +1,17 @@
-let some = new Math();
-let some2 = new Array();
-console.log(some.sqrt(5));
+// 객체로 복사
+let arr = [1, 2, 3, 4, 5];
+let someVar = {...arr};
+console.log(someVar);
+console.log(typeof someVar);
+
+// 배열로 복사.
+let arr2 = [...arr];
+arr2[2] = 12;
+console.log(arr2);
+console.log(arr);
+
+// 함수 인자로 전달
+function getTotal(a, b, c) {
+    return a + b + c;
+}
+console.log(getTotal(...[1, 2, 3]));
