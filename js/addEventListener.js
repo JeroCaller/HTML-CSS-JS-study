744af3d0f58f45dad3bfa8f08e4b00d71b296c4f
JeroCaller <j69m17@gmail.com>
2024-02-17 18:46:36 +0900

    JS - 기존 예제 수정. 이미지 관련 새 예제 추가(미완성).

--- js/addEventListener.js
+++ js/addEventListener.js
@@ -1,12 +1,13 @@
 const imageDiv = document.getElementById('image');
 const article = document.querySelector('article');
 
+/**
+ * 주어진 정수 범위 내에서 무작위로 정수 하나를 추출하여 반환하는 함수. 
+   minInt, maxInt도 범위에 포함된다.
+ * @param {number} minInt
+ * @param {number} maxInt
+*/
 function getRandomInt(minInt, maxInt) {
-    /*
-        minInt, maxInt: 정수
-        주어진 정수 범위 내에서 무작위로 정수 하나를 추출하여 반환하는 함수. 
-        minInt, maxInt도 범위에 포함된다. 
-    */
     let range = maxInt - minInt + 1;
     return Math.floor(Math.random() * range) + minInt;
 }
