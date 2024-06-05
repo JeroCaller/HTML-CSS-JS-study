1e62703749a11e9613441fdd0ac0f53807b59278
JeroCaller <j69m17@gmail.com>
2024-06-05 22:18:02 +0900

    예제 수정

--- regexp/replace.js
+++ regexp/replace.js
@@ -1,3 +1,4 @@
 let source = "i wanna wash the dish in the Washington DC";
 console.log(source.replace(/wash/gi, "clean"));
 console.log(source.replace(/wash/i, "clean"));
+console.log(source);  // 원문이 유지되는가?
